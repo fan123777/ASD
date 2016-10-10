0fd2028fb66eb78c98cd98b8bf92d386e15ffb87
fan123777 <andrey.fonaskov@gmail.com>
2016-10-10 17:57:08 +0300

    chapter5

--- RecursionAndTrees.cpp
+++ RecursionAndTrees.cpp
@@ -98,5 +98,14 @@ namespace nsDataStructures
 				// mark(l + j + i, t);
 			}
 		}
+
+		int F(int i)
+		{
+			if (i < 1)
+				return 0;
+			if (i == 1)
+				return 1;
+			return F(i - 1) + F(i - 2);
+		}
 	}
 }
