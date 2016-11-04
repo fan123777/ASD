443b1ccb71cb51e2e857f22f6ff57d13624d2c18
fan123777 <andrey.fonaskov@gmail.com>
2016-11-04 16:01:22 +0200

    chapter14

--- ST.h
+++ ST.h
@@ -792,5 +792,115 @@ namespace nsSearch
 				}
 			};
 		}
+
+		namespace nsLinearProbing
+		{
+			// Программа 14.4 Линейное зондирование
+			template <class Item, class Key>
+			class ST
+			{
+			private:
+				struct node
+				{
+					Item item;
+					node **next;
+					int sz;
+					node(Item x, int k)
+					{
+						item = x;
+						sz = k;
+						next = new node*[k];
+						for (int i = 0; i < k; i++)
+							next[i] = 0;
+					}
+				};
+
+				Item *st;
+				int N, M;
+				Item nullItem;
+
+			public:
+				ST(int maxN)
+				{
+					N = 0; M = 2 * maxN;
+					st = new Item[M];
+					for (int i = 0; i < M; i++)
+						st[i] = nullItem;
+				}
+
+				~ST()
+				{
+					delete[] st;
+				}
+
+				int count() const
+				{
+					return N;
+				}
+
+				void insert(Item item)
+				{
+					int i = hash(item.key(), M);
+					while (!st[i].null())
+						i = (i + 1) % M;
+					st[i] = item;
+					N++;
+				}
+
+				Item search(Key v)
+				{
+					int i = hash(v, M);
+					while (!st[i].null())
+					if (v == st[i].key())
+						return st[i];
+					else
+						i = (i + 1) % M;
+					return nullItem;
+				}
+
+				// Программа 14.5 Удаление из хеш - таблицы, использующей линейное зондирование
+				void remove(Item x)
+				{
+					int i = hash(x.key(), M), j;
+					while (!st[i].null())
+						if (x.key() == st[i].key())
+							break;
+						else
+							i = (i + 1) % M;
+					if (st[i].null())
+						return;
+					st[i] = nullItem;
+					N--;
+					for (j = i + 1; !st[j].null(); j = (j + 1) % M, N--)
+					{
+						Item v = st[j];
+						st[j] = nullltem;
+						insert(v);
+					}
+				}
+
+				// Программа 14.6 Двойное хеширование 
+				void insert1(Item item)
+				{
+					Key v = item.key();
+					int i = hash(v, M), k = hashtwo(v, M);
+					while (!st[i].null())
+						i = (i + k) % M;
+					st[i] = item;
+					N++;
+				}
+
+				Item search1(Key v)
+				{
+					int i = hash(v, M), k = hashtwo(v, M);
+					while (!st[i].null())
+					if (v = st[i].key())
+						return st[i];
+					else
+						i = (i + k) % M;
+					return nullItem;
+				}
+			};
+		}
 	}
 }
