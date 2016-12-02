aaeafe537c0635be1029e876b4bc10243de5f39b
fan123777 <andrey.fonaskov@gmail.com>
2016-12-02 17:50:49 +0200

    chapter17

--- Graph.cpp
+++ Graph.cpp
@@ -10,5 +10,18 @@ namespace nsAlgorithmsOnGraphs
 		{
 		
 		}
+
+		namespace nsSparseMultiGraph
+		{
+			void randE(Graph &G, int E)
+			{
+				for (int i = 0; i < E; i++)
+				{
+					int v = int(G.V()*rand() / (1.0 + RAND_MAX));
+					int w = int(G.V()*rand() / (1.0 + RAND_MAX));
+					G.insert(Edge(v, w));
+				}
+			}
+		}
 	}
 }
--- Graph.h
+++ Graph.h
@@ -7,19 +7,19 @@ namespace nsAlgorithmsOnGraphs
 {
 	namespace nsGraphPropertiesTypes
 	{
+		// Программа 17.1.Интерфейс АТД графа
+		struct Edge
+		{
+			int v;
+			int w;
+			Edge(int v = -1, int w = -1)
+				: v(v), w(w)
+			{
+			}
+		};
+
 		namespace nsGraph
 		{
-			// Программа 17.1.Интерфейс АТД графа
-			struct Edge
-			{
-				int v;
-				int w;
-				Edge(int v = -1, int w = -1)
-					: v(v), w(w)
-				{
-				}
-			};
-
 			class Graph
 			{
 			private:
@@ -98,6 +98,212 @@ namespace nsAlgorithmsOnGraphs
 				int count();
 				bool connect(int, int);
 			};
+		}
+
+		namespace nsDenseGraph
+		{
+			// Программа 17.7.Реализация АТД графа
+			class Graph
+			{
+				int Vcnt, Ecnt; bool digraph;
+				std::vector<std::vector<bool>> adj;
+
+			public:
+				Graph(int V, bool digraph = false)
+					:adj(V), Vcnt(V), Ecnt(0), digraph(digraph)
+				{
+					for (int i = 0; i < V; i++)
+						adj[i].assign(V, false);
+				}
+
+				int V() const
+				{
+					return Vcnt;
+				}
+
+				int E() const
+				{
+					return Ecnt;
+				}
+
+				bool directed() const
+				{
+					return digraph;
+				}
+
+				void insert(Edge e)
+				{
+					int v = e.v, w = e.w;
+					if (adj[v][w] == false)
+						Ecnt++;
+					adj[v][w] = true;
+					if (!digraph)
+						adj[w][v] = true;
+				}
+				void remove(Edge e)
+				{
+					int v = e.v, w = e.w;
+					if (adj[v][w] == true)
+						Ecnt--;
+					adj[v][w] = false;
+					if (!digraph)
+						adj[w][v] = false;
+				}
+
+				bool edge(int v, int w) const
+				{
+					return adj[v][w];
+				}
+
+				class adjIterator;
+				friend class adjIterator;
+			};
+
+			// Программа 17.8.Итератор для представления матрицы смежности
+			class Graph::adjIterator
+			{
+				const Graph &G;
+				int i, v;
+			public:
+				adjIterator(const Graph &G, int v)
+					:G(G), v(v), i(-1)
+				{
+				}
+
+				int begin()
+				{
+					i = -1;
+					return next();
+				}
+
+				int next()
+				{
+					for (i++; i < G.V(); i++)
+					if (G.adj[v][i] == true)
+						return i;
+					return -1;
+				}
+
+				bool end()
+				{
+					return i >= G.V();
+				}
+			};
+		}
+
+		namespace nsSparseMultiGraph
+		{
+			class Graph
+			{
+				int Vcnt, Ecnt; bool digraph;
+
+				struct node
+				{
+					int v;
+					node* next;
+					node(int x, node* t)
+					{
+						v = x;
+						next = t;
+					}
+				};
+
+				typedef node* link;
+				std::vector <link> adj;
+
+			public:
+				Graph(int V, bool digraph = false) :
+					adj(V), Vcnt(V), Ecnt(0), digraph(digraph)
+				{
+					adj.assign(V, 0);
+				}
+
+				int V() const
+				{
+					return Vcnt;
+				}
+
+				int E() const
+				{
+					return Ecnt;
+				}
+
+				bool directed() const
+				{
+					return digraph;
+				}
+
+				void insert(Edge e)
+				{
+					int v = e.v, w = e.w;
+					adj[v] = new node(w, adj[v]);
+					if (!digraph)
+						adj[w] = new node(v, adj[w]);
+					Ecnt++;
+				}
+
+				void remove(Edge e);
+
+				bool edge(int v, int w) const;
+
+				// Программа 17.10.Итератор для представления графа в виде списка смежных вершин
+				class adjlterator
+				{
+					const Graph &G;
+					int v;
+					link t;
+				public:
+					adjlterator(const Graph &G, int v) :
+						G(G), v(v)
+					{
+						t = 0;
+					}
+
+					int beg()
+					{
+						t = G.adj[v];
+						return t ? t->v : -1;
+					}
+
+					int nxt()
+					{
+						if (t) t = t->next;
+						return t ? t->v : -1;
+					}
+
+					bool end()
+					{
+						return t == 0;
+					}
+				};
+			};
+
+			// Программа 17.11.Реализация класса, определяющего степени вершин
+			template <class Graph> class Degree
+			{
+				const Graph &G;
+				std::vector <int> degree;
+
+			public:
+				Degree(const Graph &G)
+					: G(G), degree(G.V(), 0)
+				{
+					for (int v = 0; v < G.V(); v++)
+					{
+						typename Graph::adjIterator A(G, v);
+						for (int w = A.begin(); !A.end(); w = A.next())
+							degree[v]++;
+					}
+				}
+
+				int operator [] (int v) const
+				{
+					return degree[v];
+				}
+			};
+
+			// Программа 17.12.Генератор случайных графов (случайные ребра)
+			void randE(Graph &G, int E);
 		}
 	}
 }
